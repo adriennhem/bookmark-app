91808f23fa72756d3a35a728e4d6465c43c39a2b
Adrien Nhem <adrien.nhem@gmail.com>
2018-12-31 13:39:18 +0000

    update variables

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: '60PBNEHNAJ', api_key: ENV["algolia_api_key"], pagination_backend: :will_paginate  }
+AlgoliaSearch.configuration = { application_id: '60PBNEHNAJ', api_key: ENV["ALGOLIA_API_KEY"], pagination_backend: :will_paginate  }
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,8 +257,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['facebook_app_id'], ENV['facebook_app_secret']
-  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['google_client_secret']
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 
 
   # ==> Warden configuration
